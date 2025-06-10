4b36e0abbd0b6f4b7e9bd934c03e4779125b7aab
tom avilius <tomavilius@tomavilius.in>
2025-06-10 14:26:58 +0530

    Add method to scrape duck duck go.

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+// to scrape the web
+import { chromium } from "playwright";
+
+const source = "Donald Trump is dead";
+
+const element = ".kY2IgmnCmOGjharHErah";
+
+// start of scraping
+export async function scrapeDuckDuckGoNews() {
+  // launch chromium browser.
+  // BUG: Headless mode does not work.
+  // OPTIMIZE: Use headless mode.
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+
+  // search for the news
+  await page.goto(`https://duckduckgo.com/?q=news:${source}`);
+
+  try {
+    // wait for the selector (the news element)
+    await page.waitForSelector(element);
+
+    // the data from the scraping and mapping.
+    const data = await page.$$eval(element, (elements) =>
+      elements.map((el) => el.textContent?.trim() || ""),
+    );
+  } catch (error) {
+    // if an error occurs log it
+    console.log(error);
+  }
+
+  // close the browser once scraping finishes.
+  await browser.close();
+}
