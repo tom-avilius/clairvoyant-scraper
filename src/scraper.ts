9ad9fd616da7ecdb486073cd973487bf9c3e6649
tom avilius <tomavilius@tomavilius.in>
2025-06-20 20:37:09 +0530

    Add scraping of website url too.

--- src/scraper.ts
+++ src/scraper.ts
@@ -4,6 +4,7 @@ import { chromium } from "playwright";
 // the selector for news
 // WARN: This tag does change over time.
 const element = ".kY2IgmnCmOGjharHErah";
+const website = ".veU5I0hFkgFGOPhX2RBE";
 
 // start of scraping
 export async function scrapeDuckDuckGoNews(newsTitle: string): Promise<any> {
@@ -19,16 +20,25 @@ export async function scrapeDuckDuckGoNews(newsTitle: string): Promise<any> {
   try {
     // wait for the selector (the news element)
     await page.waitForSelector(element);
+    await page.waitForSelector(website);
 
     // the data from the scraping and mapping.
     // WARN: No data cleaning is performed.
     const data = await page.$$eval(element, (elements) =>
       elements.map((el) => el.textContent?.trim() || ""),
     );
+    const data2 = await page.$$eval(website, (elements) =>
+      elements.map((el) => el.textContent?.trim() || ""),
+    );
+
+    var result = [{}];
+    data.forEach((el) => {
+      result = [...result, { source: data2[data.indexOf(el)], headline: el }];
+    });
 
     // close the browser before returning from the process.
     await browser.close();
-    return data;
+    return result;
   } catch (error) {
     // if an error occurs log it
     console.log(error);
